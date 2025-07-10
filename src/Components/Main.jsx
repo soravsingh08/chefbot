80f889db687a29a185f7d7740ca037f909d5161e
soravsingh08 <rahulrakwal3@gmail.com>
2025-07-10 20:08:56 +0530

    Recipe Update

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,6 +3,7 @@ import React from "react"
 export default function Main() {
 
     const [ingredients, setIngredients] = React.useState([])
+    const [recipeShown, setRecipeShown] = React.useState(false)
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -12,12 +13,10 @@ export default function Main() {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
-    /**
-     * Challenge:
-     * Using conditional rendering, only render the new <section> IF
-     * there are ingredients added to the list of ingredients.
-     */
+
+      function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }  
 
     return (
         <main>
@@ -38,10 +37,22 @@ export default function Main() {
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
-                    <button>Get a recipe</button>
+                    <button onClick={toggleRecipeShown}>Get a recipe</button>
                 </div>
                 }
             </section>}
+
+            { recipeShown && <section className="recipe">
+        <h2>Chef's Special Pasta Recipe</h2>
+        <ol>
+            <li>Boil water and cook the pasta until al dente.</li>
+            <li>In a separate pan, brown the ground beef.</li>
+            <li>Add all the main spices and stir well.</li>
+            <li>Mix in the tomato paste and simmer for 10 minutes.</li>
+            <li>Combine pasta with the sauce and serve hot.</li>
+        </ol>
+    </section>}
+            
         </main>
     )
 }
